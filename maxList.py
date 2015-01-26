288200b76140f2fd88ecba1358d4cb28ecd685a2
Richie Barter <richie@coolgarif.com>
2015-01-26 10:41:44 +0000

    sumlist and maxlist functions

--- maxList.py
+++ maxList.py
@@ -1,15 +1,15 @@
-# Sum a list of numbers recurrsively
+# Max of a list of numbers recurrsively
 
 
 def sumlist(a,b):
 	try:
 		temp = a.pop(0)
-		b = b + temp
+		b = max(b, temp)
 		print b
 		sumlist(a,b) 
 	except IndexError:
 		return b
 	
-a = [15,45,88,96,6513,648251,77]
+a = [15,45,88888,96,6513,648251,77]
 b = 0
 sumlist(a,b)
